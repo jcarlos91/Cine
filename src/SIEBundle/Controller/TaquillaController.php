d49bafb2329c3a181c8562ba6e871f1f47e8e759
Juan Carlos <carlos91.jcc@gmail.com>
2015-12-24 12:57:47 -0600

    Cambios en taquilla

--- src/SIEBundle/Controller/TaquillaController.php
+++ src/SIEBundle/Controller/TaquillaController.php
@@ -19,21 +19,32 @@ class TaquillaController extends Controller{
 		$sala = $cartelera->getSalaId();
 		$repository = $this->getDoctrine()->getRepository('SIEBundle:Asientos');
 		$asientos = $repository->findBySala($sala);
+		//Se obtienes los limites de la sala 
 		foreach ($asientos as $key => $x) {
-			//$x = get_object_vars($value);
-			$a = $x->getFila();
-			$b = $x->getColumna();
+			$a = $x->getFila();//limite de la fila
+			$b = $x->getColumna();//limite de la columna 
 		}
-		$fil = rand(1,$a);
-		$col = rand(1,$b);
-		print_r ($postData);
+		//se obtiene la suma de los boletos
+		$totalBoletos = array_sum($postData);
+
+		$fil = rand(1,$a);//Se obtiene un nuemro ramdom para el asiento sin pasarse del limite de la fila
+		$col = rand(1,$b - $totalBoletos);//Se obtiene un nuemro ramdom para el asiento sin pasarse del limite de la columna
+		//print_r ($postData);
 		
-		for ($i=1; $i < count($postData) ; $i++) { 
+		/*
+		
+		for ($i=1; $i < count($postData); $i++) { 
 			$boleto = $postData[$i];
 			$cantidad = $postData[$i];
 		}
+
+		foreach ($postData as $boletoTipo => $cantidad) {
+			printf($boletoTipo . "=". $cantidad);
+			print("<br>");
+		}
 		print_r($boleto);
 		print_r($cantidad);
+		print_r(count($postData));*/
 		if($form->isValid() && $form->isSubmitted()){
 			try {
 				$cantidad = $postData['cantidad'];
@@ -59,8 +70,9 @@ class TaquillaController extends Controller{
 			'fil' => $fil,
 			'col' => $col,
 			'posData'=>$postData,
-			'boleto'=>$boleto,
-			'cantidad'=>$cantidad,
+			//'boleto'=>$boleto,
+			//'cantidad'=>$cantidad,
+			'totalBoletos'=>$totalBoletos,
 			)
 		);
 	}
